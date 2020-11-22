3e053676bc0d06bcb535ac5677f611cbf51cfd55
rajeevranganathan <Puttin)(*051>
2020-11-22 14:28:49 +0530

    Create Mapper

---
+++ cache/src/main/java/com/example/cache/mapper/CachedProjectMapper.kt
@@ -0,0 +1,32 @@
+package com.example.cache.mapper
+
+import com.example.cache.model.CachedProjectEntity
+import com.example.data.model.ProjectEntity
+
+class CachedProjectMapper : CacheMapper<CachedProjectEntity, ProjectEntity> {
+    override fun mapFromCache(type: CachedProjectEntity): ProjectEntity {
+        return ProjectEntity(
+            type.id,
+            type.name,
+            type.fullName,
+            type.starCount,
+            type.dateCreated,
+            type.ownerName,
+            type.ownerAvatar,
+            type.isBookmarked
+        )
+    }
+
+    override fun mapFromEntity(type: ProjectEntity): CachedProjectEntity {
+        return CachedProjectEntity(
+            type.id,
+            type.name,
+            type.fullName,
+            type.starCount,
+            type.dateCreated,
+            type.ownerName,
+            type.ownerAvatar,
+            type.isBookmarked
+        )
+    }
+}
