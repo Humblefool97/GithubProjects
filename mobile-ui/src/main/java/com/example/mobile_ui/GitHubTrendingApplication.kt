92e61c2e7b3903cd53f482795651e18c9a9a39a8
rajeevranganathan <Puttin)(*051>
2020-11-22 18:14:25 +0530

    Setup dagger

--- mobile-ui/src/main/java/com/example/mobile_ui/GitHubTrendingApplication.kt
+++ mobile-ui/src/main/java/com/example/mobile_ui/GitHubTrendingApplication.kt
@@ -1,15 +1,33 @@
 package com.example.mobile_ui
 
+import android.app.Activity
 import android.app.Application
+import com.example.mobile_ui.di.DaggerAppComponent
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasActivityInjector
 import timber.log.Timber
+import javax.inject.Inject
 
-class GitHubTrendingApplication :Application() {
+class GitHubTrendingApplication : Application(), HasActivityInjector {
+    @Inject
+    lateinit var androidInjector: DispatchingAndroidInjector<Activity>
 
     override fun onCreate() {
         super.onCreate()
         setUpTimber()
+
+        DaggerAppComponent.builder()
+            .application(this)
+            .build()
+            .inject(this)
     }
-    private fun setUpTimber(){
+
+    private fun setUpTimber() {
         Timber.plant(Timber.DebugTree())
+    }
+
+    override fun activityInjector(): AndroidInjector<Activity> {
+        return androidInjector
     }
 }
